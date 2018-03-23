49d61317d73442ff6903d22cc0b2dca9bfb3413a
sidx1024 <vistax86live@gmail.com>
2018-03-23 17:29:40 +0530

    Update links Add hash change events

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,22 +1,24 @@
 // @codekit-prepend "/vendor/hammer-2.0.8.js";
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
   // DOMMouseScroll included for firefox support
   var canScroll = true,
-      scrollController = null;
-  $(this).on('mousewheel DOMMouseScroll', function(e){
+    scrollController = null;
+  $(this).on('mousewheel DOMMouseScroll', function (e) {
 
     if (!($('.outer-nav').hasClass('is-vis'))) {
 
       e.preventDefault();
 
-      var delta = (e.originalEvent.wheelDelta) ? -e.originalEvent.wheelDelta : e.originalEvent.detail * 20;
+      var delta = (e.originalEvent.wheelDelta)
+        ? -e.originalEvent.wheelDelta
+        : e.originalEvent.detail * 20;
 
       if (delta > 50 && canScroll) {
         canScroll = false;
         clearTimeout(scrollController);
-        scrollController = setTimeout(function(){
+        scrollController = setTimeout(function () {
           canScroll = true;
         }, 100);
         updateHelper(1);
@@ -24,7 +26,7 @@ $( document ).ready(function() {
       else if (delta < -50 && canScroll) {
         canScroll = false;
         clearTimeout(scrollController);
-        scrollController = setTimeout(function(){
+        scrollController = setTimeout(function () {
           canScroll = true;
         }, 100);
         updateHelper(-1);
@@ -34,46 +36,43 @@ $( document ).ready(function() {
 
   });
 
-  $('.side-nav li, .outer-nav li').click(function(){
+  $('.side-nav li').click(function () {
 
     if (!($(this).hasClass('is-active'))) {
 
       var $this = $(this),
-          curActive = $this.parent().find('.is-active'),
-          curPos = $this.parent().children().index(curActive),
-          nextPos = $this.parent().children().index($this),
-          lastItem = $(this).parent().children().length - 1;
+        curActive = $this.parent().find('.is-active'),
+        curPos = $this.parent().children().index(curActive),
+        nextPos = $this.parent().children().index($this),
+        lastItem = $(this).parent().children().length - 1;
 
       updateNavs(nextPos);
       updateContent(curPos, nextPos, lastItem);
-
     }
 
   });
 
-  $('.cta').click(function(){
+  $('.outer-nav li').click(function () {
+    if(this.hasAttribute('data-hash')) {
+      location.hash = this.getAttribute('data-hash');
+    }
+  });
 
-    var curActive = $('.side-nav').find('.is-active'),
-        curPos = $('.side-nav').children().index(curActive),
-        lastItem = $('.side-nav').children().length - 1,
-        nextPos = 1;
+  $('.cta').click(function () {
 
-    updateNavs(lastItem);
-    updateContent(curPos, nextPos, lastItem);
+    location.hash = 1;
 
   });
 
   // swipe support for touch devices
   var targetElement = document.getElementById('viewport'),
-      mc = new Hammer(targetElement);
-  mc.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-  mc.on('swipeup swipedown', function(e) {
-
+    mc = new Hammer(targetElement);
+  mc.get('swipe').set({direction: Hammer.DIRECTION_VERTICAL});
+  mc.on('swipeup swipedown', function (e) {
     updateHelper(e);
-
   });
 
-  $(document).keyup(function(e){
+  $(document).keyup(function (e) {
 
     if (!($('.outer-nav').hasClass('is-vis'))) {
       e.preventDefault();
@@ -86,11 +85,11 @@ $( document ).ready(function() {
   function updateHelper(param) {
 
     var curActive = $('.side-nav').find('.is-active'),
-        curPos = $('.side-nav').children().index(curActive),
-        lastItem = $('.side-nav').children().length - 1,
-        nextPos = 0;
+      curPos = $('.side-nav').children().index(curActive),
+      lastItem = $('.side-nav').children().length - 1,
+      nextPos = 0;
 
-    if (param.type === "swipeup" || param.keyCode === 40 || param > 0) {
+    if (param.type === 'swipeup' || param.keyCode === 40 || param > 0) {
       if (curPos !== lastItem) {
         nextPos = curPos + 1;
         updateNavs(nextPos);
@@ -101,8 +100,8 @@ $( document ).ready(function() {
         updateContent(curPos, nextPos, lastItem);
       }
     }
-    else if (param.type === "swipedown" || param.keyCode === 38 || param < 0){
-      if (curPos !== 0){
+    else if (param.type === 'swipedown' || param.keyCode === 38 || param < 0) {
+      if (curPos !== 0) {
         nextPos = curPos - 1;
         updateNavs(nextPos);
         updateContent(curPos, nextPos, lastItem);
@@ -122,7 +121,7 @@ $( document ).ready(function() {
     $('.side-nav, .outer-nav').children().removeClass('is-active');
     $('.side-nav').children().eq(nextPos).addClass('is-active');
     $('.outer-nav').children().eq(nextPos).addClass('is-active');
-
+    location.hash = nextPos;
   }
 
   // update main content area
@@ -153,20 +152,20 @@ $( document ).ready(function() {
 
   function outerNav() {
 
-    $('.header--nav-toggle').click(function(){
+    $('.header--nav-toggle').click(function () {
 
       $('.perspective').addClass('perspective--modalview');
-      setTimeout(function(){
+      setTimeout(function () {
         $('.perspective').addClass('effect-rotate-left--animate');
       }, 25);
       $('.outer-nav, .outer-nav li, .outer-nav--return').addClass('is-vis');
 
     });
 
-    $('.outer-nav--return, .outer-nav li').click(function(){
+    $('.outer-nav--return, .outer-nav li').click(function () {
 
       $('.perspective').removeClass('effect-rotate-left--animate');
-      setTimeout(function(){
+      setTimeout(function () {
         $('.perspective').removeClass('perspective--modalview');
       }, 100);
       $('.outer-nav, .outer-nav li, .outer-nav--return').removeClass('is-vis');
@@ -177,77 +176,78 @@ $( document ).ready(function() {
 
   function workSlider() {
 
-    $('.slider--prev, .slider--next').click(function() {
+    $('.slider--prev, .slider--next').click(function () {
 
       var $this = $(this),
-          curLeft = $('.slider').find('.slider--item-left'),
-          curLeftPos = $('.slider').children().index(curLeft),
-          curCenter = $('.slider').find('.slider--item-center'),
-          curCenterPos = $('.slider').children().index(curCenter),
-          curRight = $('.slider').find('.slider--item-right'),
-          curRightPos = $('.slider').children().index(curRight),
-          totalWorks = $('.slider').children().length,
-          $left = $('.slider--item-left'),
-          $center = $('.slider--item-center'),
-          $right = $('.slider--item-right'),
-          $item = $('.slider--item');
+        curLeft = $('.slider').find('.slider--item-left'),
+        curLeftPos = $('.slider').children().index(curLeft),
+        curCenter = $('.slider').find('.slider--item-center'),
+        curCenterPos = $('.slider').children().index(curCenter),
+        curRight = $('.slider').find('.slider--item-right'),
+        curRightPos = $('.slider').children().index(curRight),
+        totalWorks = $('.slider').children().length,
+        $left = $('.slider--item-left'),
+        $center = $('.slider--item-center'),
+        $right = $('.slider--item-right'),
+        $item = $('.slider--item');
 
-      $('.slider').animate({ opacity : 0 }, 100);
+      $('.slider').animate({opacity: 0}, 100);
 
-      setTimeout(function(){
+      setTimeout(function () {
 
-      if ($this.hasClass('slider--next')) {
-        if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
-          $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-          $center.removeClass('slider--item-center').next().addClass('slider--item-center');
-          $right.removeClass('slider--item-right').next().addClass('slider--item-right');
-        }
-        else {
-          if (curLeftPos === totalWorks - 1) {
-            $item.removeClass('slider--item-left').first().addClass('slider--item-left');
+        if ($this.hasClass('slider--next')) {
+          if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 &&
+            curRightPos < totalWorks - 1) {
+            $left.removeClass('slider--item-left').next().addClass('slider--item-left');
             $center.removeClass('slider--item-center').next().addClass('slider--item-center');
             $right.removeClass('slider--item-right').next().addClass('slider--item-right');
           }
-          else if (curCenterPos === totalWorks - 1) {
-            $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-            $item.removeClass('slider--item-center').first().addClass('slider--item-center');
-            $right.removeClass('slider--item-right').next().addClass('slider--item-right');
-          }
           else {
-            $left.removeClass('slider--item-left').next().addClass('slider--item-left');
-            $center.removeClass('slider--item-center').next().addClass('slider--item-center');
-            $item.removeClass('slider--item-right').first().addClass('slider--item-right');
+            if (curLeftPos === totalWorks - 1) {
+              $item.removeClass('slider--item-left').first().addClass('slider--item-left');
+              $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+              $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+            }
+            else if (curCenterPos === totalWorks - 1) {
+              $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+              $item.removeClass('slider--item-center').first().addClass('slider--item-center');
+              $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+            }
+            else {
+              $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+              $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+              $item.removeClass('slider--item-right').first().addClass('slider--item-right');
+            }
           }
-        }
-      }
-      else {
-        if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
-          $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-          $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-          $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
         }
         else {
-          if (curLeftPos === 0) {
-            $item.removeClass('slider--item-left').last().addClass('slider--item-left');
-            $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-            $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
-          }
-          else if (curCenterPos === 0) {
+          if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
             $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-            $item.removeClass('slider--item-center').last().addClass('slider--item-center');
+            $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
             $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
           }
           else {
-            $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
-            $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
-            $item.removeClass('slider--item-right').last().addClass('slider--item-right');
+            if (curLeftPos === 0) {
+              $item.removeClass('slider--item-left').last().addClass('slider--item-left');
+              $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+              $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+            }
+            else if (curCenterPos === 0) {
+              $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+              $item.removeClass('slider--item-center').last().addClass('slider--item-center');
+              $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+            }
+            else {
+              $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+              $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+              $item.removeClass('slider--item-right').last().addClass('slider--item-right');
+            }
           }
         }
-      }
 
-    }, 100);
+      }, 100);
 
-    $('.slider').animate({ opacity : 1 }, 100);
+      $('.slider').animate({opacity: 1}, 100);
 
     });
 
@@ -255,11 +255,11 @@ $( document ).ready(function() {
 
   function transitionLabels() {
 
-    $('.work-request--information input').focusout(function(){
+    $('.work-request--information input').focusout(function () {
 
       var textVal = $(this).val();
 
-      if (textVal === "") {
+      if (textVal === '') {
         $(this).removeClass('has-value');
       }
       else {
@@ -277,4 +277,15 @@ $( document ).ready(function() {
   workSlider();
   transitionLabels();
 
+  window.onhashchange = function (hashChangeEvent) {
+    var oldURL = hashChangeEvent.oldURL;
+    var newURL = hashChangeEvent.newURL;
+    var oldHash = oldURL.slice(oldURL.lastIndexOf('#')).slice(1);
+    var newHash = newURL.slice(newURL.lastIndexOf('#')).slice(1);
+    var totalItems = $('.side-nav li').parent().children().length;
+    var lastItem = totalItems - 1;
+    if (newHash < totalItems) {
+      updateContent(oldHash, newHash, lastItem);
+    }
+  };
 });
